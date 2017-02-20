4dd55996b0b7bd90f563439a76062e838ab76986
carotene4035 <karoten512@gmail.com>
2017-02-20 00:59:54 +0000

    first commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,61 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy]
+  # 表示用メソッド
+  def index
+    @blogs = Blog.all
+  end
+  
+  def new
+    if params[:back]
+      @blog = Blog.new(blogs_params)
+    else
+      @blog = Blog.new
+    end
+  end
+  
+  def edit
+  end
+  
+  # 処理呼び出し用メソッド
+  def create
+    blog = Blog.new(blogs_params)
+    if blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました!"
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    if @blog.update(blogs_params)
+      redirect_to blogs_path, notice: "ブログを編集しました!"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    # インスタンス変数に代入するのはなぜか
+    # 普通の変数ではダメなのか
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました!"
+  end
+  
+  def confirm
+    @blog = Blog.new(blogs_params)
+    render :new if @blog.invalid?
+    
+  end
+ 
+ private
+ 
+ def blogs_params
+   # 必要なパラメータのみを安全に切り取る
+   # それがストロングパラメータ
+   params.require(:blog).permit(:title, :content)
+ end
+ 
+ def set_blog
+   @blog = Blog.find(params[:id])
+ end
+end
