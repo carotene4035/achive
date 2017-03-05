6e4425d82ecaae2ff6165faa7b7b8e50c9ca7f8b
carotene4035 <karoten512@gmail.com>
2017-03-05 04:22:35 +0000

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 100.times do |n|
-#   email = Faker::Internet.email
-#   password = "password"
-#   name = Faker::Name.name
-#   User.create!(email: email,
-#                name: name,
-#                password: password,
-#                password_confirmation: password,
-#                )
-# end
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
+end
 
 n = 1
 while n <= 100
