dcdcc0d48013e2d66bcd33a89c6cd9ac1d902e6a
carotene512 <karoten512@gmail.com>
2017-05-26 16:23:33 +0900

    既読・未読として表示するように成形。

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,12 @@ module NotificationsHelper
   def posted_time(time)
     time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
   end
+
+  def already_read(boolean)
+    if boolean == true
+      '既読'
+    elsif boolean == false
+      '未読'
+    end
+  end
 end
